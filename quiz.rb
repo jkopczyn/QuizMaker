73902a44b38df575b65d0e2fbf0c19892cb31cfc
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-09-21 18:36:27 -0700

    too much Python recently

--- quiz.rb
+++ quiz.rb
@@ -1,5 +1,5 @@
-class Classroom:
-    def initialize(filename=nil):
+class Classroom
+    def initialize(filename=nil)
         @strands = {}
         @users = {}
         if filename
@@ -8,13 +8,13 @@ class Classroom:
         end
     end
 
-    def generate_quiz_for_user(id, how_many):
+    def generate_quiz_for_user(id, how_many)
         user = @users[id]
         return [0]*how_many if user
     end
 
     private
-    def parse(file):
+    def parse(file)
         expected_header = [:strand_id,:strand_name,:standard_id,:standard_name,:question_id,:difficulty]
         header = file.readline.split(",").map(:to_sym)
         raise "Bad Format" unless header == expected_header
